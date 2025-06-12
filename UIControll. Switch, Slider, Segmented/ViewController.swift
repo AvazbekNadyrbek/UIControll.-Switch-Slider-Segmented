3b7fe676ffab59c79a7d289de549f3215ef33c18
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-06-12 20:00:40 +0200

    Here one time change

--- UIControll. Switch, Slider, Segmented/ViewController.swift
+++ UIControll. Switch, Slider, Segmented/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var btn = CustomButton(frame: CGRect(x: 30, y: 100, width: 200, height: 200), image: UIImage(systemName: "plus")!, action: actionBtn)
+    lazy var actionBtn: UIAction = UIAction { _ in
+        print("Button is pressed")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        view.addSubview(btn)
     }
+}
 
-
+class CustomButton: UIControl {
+    
+    lazy var imageView: UIImageView = {
+        $0.contentMode = .scaleAspectFit
+        $0.clipsToBounds = true
+        $0.frame.origin = .zero
+        $0.frame.size = frame.size
+       return $0
+    }(UIImageView())
+    
+    init(frame: CGRect,image: UIImage, action: UIAction) {
+        super.init(frame: frame)
+        self.imageView.image = image
+        addAction(action, for: .touchUpInside)
+        addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
