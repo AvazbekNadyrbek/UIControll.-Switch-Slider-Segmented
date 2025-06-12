e4f412f47c65f74eb28704fe2470b8118eddcf27
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-06-12 17:00:50 +0200

    Initial Commit

---
+++ UIControll. Switch, Slider, Segmented/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIControll. Switch, Slider, Segmented
+//
+//  Created by Авазбек Надырбек уулу on 12.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
